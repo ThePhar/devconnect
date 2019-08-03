89ac3aea970e3e866d40a80d12796dc69635d076
Zach Parks <zach@alliware.com>
2019-08-02 22:05:52 -0500

    adds new user with correct POST to /api/users

--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
-const mongoose = require('mongoose');
-const config = require('config');
+const mongoose = require("mongoose");
+const config = require("config");
 
-const db = config.get('mongoURI');
+const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, { useNewUrlParser: true });
-    console.log('MongoDB connected...');
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true
+    });
+    console.log("MongoDB connected...");
   } catch (err) {
     console.log(err.message);
     // Exit application with failure.
