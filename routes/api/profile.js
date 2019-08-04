eb715dc3b55d4ac81403a8d85ba0cd189029cd31
Zach Parks <zach@alliware.com>
2019-08-03 19:13:38 -0500

    added profile api endpoint to add experience entry

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -13,6 +13,17 @@ const updateProfileHandler = [
     .not()
     .isEmpty()
 ];
+const experienceHandler = [
+  check("title", "Title is required")
+    .not()
+    .isEmpty(),
+  check("company", "Company is required")
+    .not()
+    .isEmpty(),
+  check("from", "From date is required")
+    .not()
+    .isEmpty()
+];
 
 // @route    GET api/profile/me
 // @desc     Get current user's profile
@@ -150,6 +161,40 @@ router.delete("/", auth, async (req, res) => {
     await User.findOneAndRemove({ _id: req.user.id });
 
     return res.json({ msg: "User deleted" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+// @route    PUT api/profile/experience
+// @desc     Add profile experience
+// @access   Private
+router.put("/experience", [auth, experienceHandler], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty())
+    return res.status(400).json({ errors: errors.array() });
+
+  const { title, company, location, from, to, current, description } = req.body;
+
+  const newExperience = {
+    title,
+    company,
+    location,
+    from,
+    to,
+    current,
+    description
+  };
+
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    profile.experience.unshift(newExperience);
+
+    await profile.save();
+
+    res.json(profile);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");
